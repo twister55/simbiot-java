35a41ebf3470591c8cafae5b5a5f4b01e87b7bfa
Vadim Yelisseyev <vadim.yelisseyev@gmail.com>
2021-03-24 17:23:15 +0300

    compiler refactoring

--- simbiot-compiler/src/main/java/dev/simbiot/compiler/Compiler.java
+++ simbiot-compiler/src/main/java/dev/simbiot/compiler/Compiler.java
@@ -1,17 +1,22 @@
 package dev.simbiot.compiler;
 
 import java.io.File;
+import java.io.IOException;
 
 import dev.simbiot.Component;
+import dev.simbiot.ComponentProvider;
 import dev.simbiot.Props;
 import dev.simbiot.Slots;
 import dev.simbiot.Writer;
 import dev.simbiot.ast.Program;
+import dev.simbiot.ast.ProgramLoader;
 import dev.simbiot.compiler.bytecode.ArrayField;
 import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.description.modifier.FieldManifestation;
+import net.bytebuddy.description.modifier.Visibility;
 import net.bytebuddy.dynamic.DynamicType.Unloaded;
 import net.bytebuddy.dynamic.scaffold.InstrumentedType;
+import net.bytebuddy.implementation.FieldAccessor;
 import net.bytebuddy.implementation.Implementation;
 import net.bytebuddy.implementation.bytecode.ByteCodeAppender;
 import net.bytebuddy.implementation.bytecode.StackManipulation;
@@ -22,67 +27,89 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 /**
  * @author <a href="mailto:vadim.yelisseyev@gmail.com">Vadim Yelisseyev</a>
  */
-public class Compiler {
+public class Compiler implements ComponentProvider {
     public static final String CONSTANTS_FIELD_NAME = "$$CONSTANTS";
+    public static final String COMPONENTS_FIELD_NAME = "$$components";
 
-    private final ProgramProcessor processor = new ProgramProcessor();
+    private final ProgramLoader<?> loader;
+    private final ProgramProcessor processor;
 
-    public Component compile(String id, Program program) {
+    public Compiler(ProgramLoader<?> loader) {
+        this.loader = loader;
+        this.processor = new ProgramProcessor();
+    }
+
+    @Override
+    public Component getComponent(String id) throws IOException {
+        final ProcessContext context = new ProcessContext(id);
+
+        return createInstance(loadClass(context));
+    }
+
+    private Component createInstance(Class<? extends Component> type) {
         try {
-            final Unloaded<Component> unloaded = process(id, program);
-
-            unloaded.saveIn(new File("generated"));
-
-            return unloaded
-                .load(Compiler.class.getClassLoader())
-                .getLoaded()
-                .newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException("Error while creating component", e);
+            return type.newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public Unloaded<Component> process(String id, Program program) {
-        final ProcessContext context = new ProcessContext();
+    private Class<? extends Component> loadClass(ProcessContext context) throws IOException {
+        return compile(context)
+            .load(Component.class.getClassLoader())
+            .getLoaded();
+    }
 
-        return new ByteBuddy()
+    private Unloaded<Component> compile(ProcessContext context) throws IOException {
+        final Unloaded<Component> unloaded = new ByteBuddy()
             .subclass(Component.class)
-            .name(id)
+            .name(context.getId())
             .defineField(CONSTANTS_FIELD_NAME, of(byte[][].class), Opcodes.ACC_PRIVATE | Opcodes.ACC_STATIC | Opcodes.ACC_FINAL)
+            .defineField(COMPONENTS_FIELD_NAME, of(Component[].class), Visibility.PRIVATE, FieldManifestation.PLAIN)
+            .defineConstructor(Visibility.PUBLIC)
+            .withParameters(Component[].class)
+            .intercept(FieldAccessor.ofField(COMPONENTS_FIELD_NAME).setsArgumentAt(0))
             .method(named("render"))
-            .intercept(new Implementation() {
-                @Override
-                public ByteCodeAppender appender(Target target) {
-                    return (mv, ctx, method) -> new ByteCodeAppender.Size(
-                        process(context, program, ctx.getInstrumentedType()).apply(mv, ctx).getMaximalSize(),
+            .intercept(renderMethod(context, loader.load(context.getId())))
+            .initializer(staticInitializer(context))
+            .make();
+
+        unloaded.saveIn(new File("generated"));
+
+        return unloaded;
+    }
+
+    private ByteCodeAppender staticInitializer(ProcessContext context) {
+        return (visitor, ctx, method) -> {
+            final StackManipulation.Size size = new ArrayField(
+                ctx.getInstrumentedType(), CONSTANTS_FIELD_NAME, context.getConstants()
+            ).apply(visitor, ctx);
+
+            return new ByteCodeAppender.Size(size.getMaximalSize(), method.getStackSize());
+        };
+    }
+
+    private Implementation renderMethod(ProcessContext context, Program program) {
+        return new Implementation() {
+            @Override
+            public ByteCodeAppender appender(Target target) {
+                return (mv, ctx, method) -> {
+                    context.fields(ctx.getInstrumentedType().getDeclaredFields());
+                    context.store("writer", Writer.class);
+                    context.store("props", Props.class);
+                    context.store("slots", Slots.class);
+
+                    return new ByteCodeAppender.Size(
+                        processor.process(context, program).apply(mv, ctx).getMaximalSize(),
                         method.getStackSize() + context.getLocalVarsCount()
                     );
-                }
+                };
+            }
 
-                @Override
-                public InstrumentedType prepare(InstrumentedType type) {
-                    return type;
-                }
-            })
-            .initializer((visitor, ctx, method) -> {
-                final StackManipulation.Size size = new StackManipulation.Compound(
-                    new ArrayField(ctx.getInstrumentedType(), CONSTANTS_FIELD_NAME, context.getConstants())
-                ).apply(visitor, ctx);
-
-                return new ByteCodeAppender.Size(size.getMaximalSize(), method.getStackSize());
-            })
-            .make();
-    }
-
-    protected StackManipulation.Compound process(ProcessContext context, Program program, TypeDescription type) {
-        prepareContext(context, type);
-        return processor.process(context, program);
-    }
-
-    protected void prepareContext(ProcessContext context, TypeDescription type) {
-        context.fields(type.getDeclaredFields());
-        context.store("writer", Writer.class);
-        context.store("props", Props.class);
-        context.store("slots", Slots.class);
+            @Override
+            public InstrumentedType prepare(InstrumentedType type) {
+                return type;
+            }
+        };
     }
 }
